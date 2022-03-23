7fc05db6a16b7056ce12789145744a6e3f75ced2
ilhamhmmd <ilhamhd98@gmail.com>
2022-03-24 06:36:04 +0700

    update inmemory notes service

--- src/services/inMemory/NotesService.js
+++ src/services/inMemory/NotesService.js
@@ -1,4 +1,6 @@
 const { nanoid } = require('nanoid');
+const InvariantError = require('../../exceptions/InvariantError');
+const NotFoundError = require('../../exceptions/NotFoundError');
 
 class NotesService {
     constructor() {
@@ -8,7 +10,7 @@ class NotesService {
     addNote({ title, body, tags}) {
         const id = nanoid(16);
         const createdAt = new Date().toISOString();
-        const updatedAt = new createdAt;
+        const updatedAt = createdAt;
 
         const newNote = {
             title, tags, body, id, createdAt, updatedAt
@@ -19,7 +21,7 @@ class NotesService {
         const isSuccess = this._notes.filter((note) => note.id === id).length > 0;
 
         if (!isSuccess) {
-            throw new Error('Catatan gagal ditambahkan!');
+            throw new InvariantError('Catatan gagal ditambahkan!');
         }
 
         return id;
@@ -33,7 +35,7 @@ class NotesService {
         const note = this._notes.filter((n) => n.id === id)[0];
 
         if (!note) {
-            throw new Error('Catatan tidak ditemukan!');
+            throw new NotFoundError('Catatan tidak ditemukan');
         }
 
         return note;
@@ -43,7 +45,7 @@ class NotesService {
         const index = this._notes.findIndex((note) => note.id === id);
 
         if (index === -1) {
-            throw new Error('Gagal memperbarui catatan. Id tidak ditemukan!')
+            throw new NotFoundError('Gagal memperbarui catatan. Id tidak ditemukan!')
         }
 
         const updatedAt = new Date().toISOString();
@@ -62,7 +64,7 @@ class NotesService {
         const index = this._notes.findIndex((note) => note.id === id);
 
         if (index === -1) {
-            throw new Error('Catatan gagal dihapus. Id tidak ditemukan!');
+            throw new NotFoundError('Catatan gagal dihapus. Id tidak ditemukan!');
         }
 
         this._notes.splice(index, 1);
